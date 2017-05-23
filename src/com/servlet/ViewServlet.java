28b7d9b5381a8adf4edb2dd55f1ce1a3e8ba26c5
santoshsahoo365 <santoshsahoo365@gmail.com>
2017-05-23 13:01:45 +0530

    first commit

---
+++ src/com/servlet/ViewServlet.java
@@ -0,0 +1,49 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewServlet
+ */
+@WebServlet("/ViewServlet")
+public class ViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ViewServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hey in get");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+System.out.println("in post");
+		PrintWriter out = response.getWriter();
+			String name = request.getParameter("txtname");
+			int age = Integer.parseInt(request.getParameter("txtage"));
+			out.println("Welcome to the world of Servlet");
+			out.println("Name is:" + name);
+			out.println("Age is:" + age);
+		
+	}
+
+}
